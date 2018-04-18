a1f91684285fbdaec3aab236304ee746750ab73f
aonemd <aonemdsaleh@gmail.com>
2018-04-18 23:49:33 +0200

    Set up postcss

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,3 @@
+import styles from '../css/main.css'
+
 console.log("I'm Jack");
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
       }
     ]
   }
