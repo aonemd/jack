cf85aca8c9d16b4d0c882399267d7b2487bcb9c8
aonemd <aonemdsaleh@gmail.com>
2018-04-19 00:32:09 +0200

    Build css and js into index.js in the root build folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, 'src/assets/js/main.js'),
   output: {
-    path: path.resolve(__dirname, 'src/assets'),
+    path: path.resolve(__dirname, 'src/'),
     filename: 'index.js'
   },
   module: {
